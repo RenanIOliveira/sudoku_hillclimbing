1fd3aef213145e7ffbda7989f3f7a0eb53a1532d
Eduardo Leao <eduardotleao@gmail.com>
2019-12-04 00:35:45 -0300

    hill climbing finalizado com revisao pendente

--- hill_climbing/sudoku_hill_climbing.cpp
+++ hill_climbing/sudoku_hill_climbing.cpp
@@ -5,6 +5,7 @@
 #include <map>
 #include <vector>
 #include <bits/stdc++.h>
+#include <algorithm>
 
 using namespace std;
 
@@ -20,7 +21,7 @@ int board[81] = {0,0,0,7,0,0,0,0,0,
                  0,0,2,0,0,0,0,5,0,
                  0,4,0,0,0,0,3,0,0};
 
-const int fixed_positions[] = {3,  9,  21, 22, 24, 35, 39, 41, 51, 52, 53, 58, 59, 65, 70, 73, 78};
+int fixed_positions[] = {3,  9,  21, 22, 24, 35, 39, 41, 51, 52, 53, 58, 59, 65, 70, 73, 78};
 
 const int squares[9][9] = {{0,1,2,9,10,11,18,19,20},{3,4,5,12,13,14,21,22,23},{6,7,8,15,16,17,24,25,26},
                          {27,28,29,36,37,38,45,46,47},{30,31,32,39,40,41,48,49,50},{33,34,35,42,43,44,51,52,53},
@@ -30,28 +31,50 @@ int * fill_board(int * board);
 int repete_linhas(int * board);
 int repete_colunas(int * board);
 int repete_squares(int * board);
-
+int * choose_son(int * board, int position);
+void print_board(int * board);
 
 int objetivo(int * board);
 
 int main() {
 
   int no_corrente[81];
+  int novo[81];
 
   memcpy(no_corrente,fill_board(board), sizeof(int[81]));
 
-  for (int i = 0; i < 81; i++) {
-    if (i % 9 == 8) {
-      cout << no_corrente[i] << '\n';
-    }
-    else{
-      cout << no_corrente[i] << ", " ;
-    }
-  };
+  cout << "Sudoku inicial:" << objetivo(no_corrente) << '\n';
 
-  cout << objetivo(no_corrente) << '\n';
+  print_board(no_corrente);
+
+  cout << "Função objetivo:" << objetivo(no_corrente) << "\n\n";
+
+  for (int i = 0; i < 81; i++) {
+    if (objetivo(no_corrente) == 0)
+      break;
+    else{
+      memcpy(no_corrente,choose_son(no_corrente,i), sizeof(int[81]));
+    }
+  }
+
+  cout << "Minimo encontrado:" << '\n';
+
+  print_board(no_corrente);
+
+  cout << "Função objetivo:" << objetivo(no_corrente) << '\n';
 
   return 0;
+}
+
+void print_board(int * board) {
+  for (int i = 0; i < 81; i++) {
+    if (i % 9 == 8) {
+      cout << board[i] << '\n';
+    }
+    else{
+      cout << board[i] << ", " ;
+    }
+  };
 }
 
 int * fill_board(int * board){
@@ -139,4 +162,25 @@ int repete_squares(int * board){
   }
 //  cout << "total: " << total << '\n';
   return total;
+}
+
+int * choose_son(int * board,int position){
+  int compara[81];
+  int * p;
+  int valor_atual = board[position];
+  int i;
+  memcpy(compara, board, sizeof(int[81]));
+  p = find (fixed_positions, fixed_positions+17, position);
+  if (p != fixed_positions+17)
+    return board;
+  else{
+    for (i = 1; i <= 9; i++) {
+      if (valor_atual != i) {
+        compara[position] = i;
+        if (objetivo(compara) < objetivo(board))
+          memcpy(board, compara, sizeof(int[81]));
+      }
+    }
+  }
+  return board;
 }
