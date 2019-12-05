809905cab307be40c97b9b0a30837951dd112702
rodcf <rcf2504@gmail.com>
2019-12-05 13:42:56 -0300

    Implementando Roulette Wheel Selection

--- genetico/genetic.js
+++ genetico/genetic.js
@@ -10,13 +10,25 @@ function generateInitialPopulation(){
 
 var crossoverProbability = 0.8;
 var mutationProbability = 0.01;
-var populationSize = 200;
+var populationSize = 100;
 var maxGenerations = 300;
 var elitism = true;
 
 function randomValue(min, max) {
 
     return Math.random() * (max - min) + min;
+}
+
+function calcFitness(population) {
+
+    fitness = []
+    // population.sort(function(a, b){return b.fitness() - a.fitness()});
+
+    for(individual of population) {
+        fitness.push(individual.fitness())
+    }
+
+    return fitness
 }
 
 function fitnessSum(population) {
@@ -29,14 +41,57 @@ function fitnessSum(population) {
     return totalFitness;
 }
 
+// Roulette Wheel Selection
+function rouletteWheel(population) {
+
+    population.sort(function(a, b){return b.fitness() - a.fitness()});
+    var totalFitness = fitnessSum(population);
+
+    relativeFitness = [];
+
+    for (individual of population)
+        relativeFitness.push(individual.fitness()/totalFitness);
+
+    var probabilities = [];
+
+    relativeFitness.reduce(function(a,b,i) { return probabilities[i] = a+b; },0);
+
+    // console.log(calcFitness(population))
+
+    // console.log(relativeFitness)
+
+    // console.log(probabilities)
+
+    var parents = [];
+
+    while (parents.length < populationSize) {
+
+        var pick = randomValue(0, probabilities[populationSize-1]);
+        var current = 0;
+
+        for (individual in population) {
+
+            current += probabilities[individual];
+
+            if (pick <= current) {
+                parents.push(population[individual]);
+                break;
+            }
+        }
+    }
+
+    parents.sort(function(a, b){return b.fitness() - a.fitness()});
+    return parents;
+}
+
 // Stochastic Universal Sampling (SUS)
 function selection(population) {
 
-    population.sort(function(a, b){return a.fitness() - b.fitness()});
+    population.sort(function(a, b){return b.fitness() - a.fitness()});
 
     var totalFitness = fitnessSum(population);
 
-    var numberOfParents = populationSize / 2;
+    var numberOfParents = populationSize;
     var distance = totalFitness / numberOfParents;
     points = [];
     startPoint = randomValue(0, distance);
@@ -59,8 +114,13 @@ function selection(population) {
         while ( fitnessSum(population.slice(0,i)) < p) {
             i++;
         }
-        parents.push(population[i]);
+
+        if(population[i])
+            parents.push(population[i]);
+        
     }
+
+    console.log("length: ", parents.length)
     
     return parents;
 }
@@ -70,11 +130,18 @@ genetic()
 function genetic(){
 
     var population = generateInitialPopulation();
-    
+    var parents;
 
-    var parents = selection(population);
-    console.log(parents)
-    
+    var generations = 0;
+
+    parents = rouletteWheel(population);
+
+    console.log(calcFitness(parents))
+
+    // while (generations < maxGenerations) {
+
+
+    // }
 }
 
 
